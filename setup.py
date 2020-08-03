c324df9615f42d92a94b97728aa4d61930381b06
Sriram G <marirs@gmail.com>
2020-08-03 09:55:34 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,8 +1,37 @@
-from distutils.core import setup
+"""
+Allwhois
+---------
+Get Whois information for any ccTLD or TLD using the linux/macos
+package "whois".
+
+allwhois is a wrapper for the system whois package.
+
+Source code is hosted on `GitHub <https://github.com/marirs/allwhois>`
+Contributions are welcome!
+"""
+from setuptools import find_packages, setup
 
 keywords = [
     'Python 3', 'whois', 'tld', 'domain', 'expiration', 'cctld',
     'domainer', '.com', 'registrar', 'allwhois', 'anywhois'
+]
+
+tests_require = [
+    'coverage>=4.5',
+    'codecov>=2.1.7',
+    'pytest>=5.2',
+    'pytest-cov>=2.8',
+]
+
+extras_require = {
+    'tests': tests_require,
+}
+
+extras_require['all'] = [req for exts, reqs in extras_require.items()
+                         for req in reqs]
+
+setup_requires = [
+    'pytest-runner>=5.2',
 ]
 
 with open('README.md') as f:
@@ -20,7 +49,8 @@ setup(
     license='MIT',
     url='https://github.com/marirs/allwhois/',
     platforms=['posix'],
-    packages=['allwhois'],
+    zip_safe=False,
+    packages=find_packages(),
     include_package_data=True,
     keywords=keywords,
     classifiers=[
