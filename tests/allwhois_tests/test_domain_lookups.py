cd846f25dfeeb7801eba1593568e9e13e11dce2d
Sriram G <marirs@gmail.com>
2020-08-03 09:40:18 +0530

    test domain_info

--- tests/allwhois_tests/test_domain_lookups.py
+++ tests/allwhois_tests/test_domain_lookups.py
@@ -14,26 +14,26 @@ class TestDomainLookups:
         result = whois.query(domain, date_as_string=True)
         assert result["msg"] == "success"
         assert result["domain_info"]["name"].lower() == "google.com"
-        assert result == lookup_results['google_com']
+        assert result["domain_info"] == lookup_results['google_com']["domain_info"]
 
     def test_google_net(self):
         domain = "google.net"
         result = whois.query(domain, date_as_string=True)
         assert result["msg"] == "success"
         assert result["domain_info"]["name"].lower() == "google.net"
-        assert result == lookup_results['google_net']
+        assert result["domain_info"] == lookup_results['google_net']["domain_info"]
 
     def test_google_org(self):
         domain = "google.org"
         result = whois.query(domain, date_as_string=True)
         assert result["msg"] == "success"
         assert result["domain_info"]["name"].lower() == "google.org"
-        assert result == lookup_results['google_org']
+        assert result["domain_info"] == lookup_results['google_org']["domain_info"]
 
     def test_example_com(self):
         domain = "example.com"
         result = whois.query(domain, date_as_string=True)
         assert result["msg"] == "success"
         assert result["domain_info"]["name"].lower() == "example.com"
-        assert result == lookup_results['example_com']
+        assert result["domain_info"] == lookup_results['example_com']["domain_info"]
 
